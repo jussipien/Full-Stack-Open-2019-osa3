121a85e5ac9b6853466f1a1ac847945fd116b48a
Jussi Pienisaari <jussi.pienisaari@insolution.fi>
2019-12-10 19:27:42 +0200

    Done 3.8

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
+const PORT = 3001
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [{
                   "name": "Arto Hellas",
@@ -27,11 +35,67 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello!</h1>')
 })
 
+app.get('/info', (req, res) => {
+  let length = persons.length
+  let date = new Date()
+
+  res.send(`<p>Phonebook has info for ${length} people</p>
+            <p>${date}</p>`)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-const PORT = 3001
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+
+  if (!!person) {
+    res.json(person)
+  } else {
+    res.status(404)
+    res.send(`Person with id ${id} not found`)
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  let personPost = req.body
+  console.log(personPost)
+
+  if (!personPost.name) {
+    res.status(400).json({'error':'Name input was empty!'})
+  } else if (!personPost.number) {
+    res.status(400).json({'error': 'Number input was empty!'})
+  } else if (persons.find(person => person.name === personPost.name)) {
+    res.status(409).json({'error':`${personPost.name} already exists in phonebook!`})
+  } else {
+    let idInUse = true
+    let randomId
+
+    while (idInUse) {
+      randomId = Math.floor(Math.random()*999999)
+      console.log(randomId)
+      if (!persons.find(person => person.id === randomId)) {
+        idInUse = false
+      }
+    }
+    
+    personPost.id = randomId
+    persons.push(personPost)
+    res.status(201).send(`${personPost.name} added to phonebook with number ${personPost.number}`)
+  }
+  
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

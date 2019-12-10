2f4718c55e428ca88e0fc1fc51df9de4c8d8f8e1
Jussi Pienisaari <jussi.pienisaari@insolution.fi>
2019-12-10 20:15:08 +0200

    Added Procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
